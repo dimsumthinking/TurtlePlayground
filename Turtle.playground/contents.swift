4bb795b1fe4b177f384f1e8fb555f6d11384231c
Daniel Steinberg <daniel@dimsumthinking.com>
2015-06-02 14:00:55 -0400

    pushing deleted file - we'll add a Cosmic Turtle later!

--- Turtle.playground/contents.swift
+++ Turtle.playground/contents.swift
@@ -1,4 +1,14 @@
-
+import UIKit
 
 turtle
 
+let avatar = Avatar()
+
+avatar.view
+
+let myView = UIView(frame: CGRect(x: 0, y: 0, width: 500, height: 500))
+myView.backgroundColor = UIColor.lightGrayColor()
+myView.addSubview(avatar.viewAt(x: 250, y: 0, withHeadingInRadians: -0.0))
+
+
+myView
