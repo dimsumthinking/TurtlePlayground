8899179fdb345ecdb519f444756359c2846cf5a3
Daniel Steinberg <daniel@dimsumthinking.com>
2015-06-02 14:00:09 -0400

    Added Avatar class to work with Turtle

--- Turtle.playground/Sources/Secret Turtle Stuff/Avatar.swift
+++ Turtle.playground/Sources/Secret Turtle Stuff/Avatar.swift
@@ -1,2 +1,18 @@
-import Foundation
+import UIKit
 
+public struct Avatar {
+    public var view = UIImageView(image: UIImage(named: "Turtle.png"))
+    
+    public init(){}
+    
+    public func viewAt(# x: Double, y: Double, withHeadingInRadians radians: Double) -> UIImageView {
+        let rotateByRadians = CGAffineTransformMakeRotation(CGFloat(radians))
+        let moveTurtle = CGAffineTransformMakeTranslation(CGFloat(x) - view.frame.size.width/2, CGFloat(y) - view.frame.size.height/2)
+        let turtleTransform =  CGAffineTransformConcat(rotateByRadians, moveTurtle)
+        view.transform = turtleTransform
+        println(view.frame)
+
+        return view
+    }
+    
+}
