671a5810adac081fcdc7c793fa885ee30e02ea3e
Daniel Steinberg <daniel@dimsumthinking.com>
2015-06-06 11:23:47 -0400

    Removed Spirograph support for now.

--- Turtle.playground/Sources/What a Turtle Understands/TurtleCurves.swift
+++
@@ -1,8 +0,0 @@
-//
-//public func curveTo(x: Double, y: Double, withHeading: Double) {
-//    turtle.curveTo(x:x, y:y, degrees:withHeading)
-//}
-//
-//public func loopWithAngle(degrees: Double){
-//    turtle.loopWithAngle(degrees)
-//}
