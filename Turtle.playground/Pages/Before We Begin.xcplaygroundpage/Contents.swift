63b659f26fd16ea7e6c347044edb1ad9f97372d6
Daniel Steinberg <daniel@dimsumthinking.com>
2015-08-12 09:04:49 -0400

    Added documentation

--- Turtle.playground/Pages/Before We Begin.xcplaygroundpage/Contents.swift
+++ Turtle.playground/Pages/Before We Begin.xcplaygroundpage/Contents.swift
@@ -8,6 +8,26 @@ We need to save the position of our target or it will constantly be moving aroun
 * Find your `Documents` director. 
 * Add a new folder named `Shared Playground Data` making sure that there's a space between each word.
 
+In each playground, begin by typing in the word turtle. You will also need to type return so that you get a new line.
+
+![Turtle1](Turtle1.jpg)
+
+On the right side of the playground you should get some feedback that shows the name of the playground page and the word "Turtle". Hover over this with the cursor and you will see two icons. The right icon is the "Show Result" icon.
+
+![Turtle2](Turtle2.jpg)
+
+Click the Show Result icon and a Turtle should appear in its environment. Tap on the environment and resize it as needed. Remember, you need to hit return after the turtle line so that you have an additional line or you won't see the full turtle environment.
+
+![Turtle3](Turtle3.jpg)
+
+In this sequence of pages, you will expand what the turtle can do.
+
+* Linear Turtle with Target - the turtle can move `forward()` and `back()`. Move it to the target. The background color turns yellow when you've reached the target. To reset the target, remove everything from the playground and type `resetTarget()`. Once the target is reset, remove the line `resetTarget()` and add turtle back in.
+* Turtle with Target - the turtle can also move `right()` and `left()`.
+* Turtle with Parameters and Target - the four functions can take parameters. You can now say `forward(150)`. The default value for distance is 50. The default value for angle rotation is 90. So `right()` is the same as `right(90)`.
+* Cosmic Turtle and Target - the turtle and its path can now take on different colors. Use `penColor()` to specify a color and `nextColor()` to rotate through them. The colors are .Red, .Orange, .Yellow, .PeaGreen, .Green, .BlueGreen, .Cyan, .Cerullian, .Blue, .Purple, .Magenta, and .Pink
+* Turtle - you can now `show()` and `hide()` the turtle and enable and disable the pen using `penDown()` and `penUp()`. This allows you to draw dotted lines and any shape you can think of.
+
 Enjoy!
  
 ---
--- Turtle.playground/Pages/Linear Turtle with Target Hint.xcplaygroundpage/Contents.swift
+++ Turtle.playground/Pages/Linear Turtle with Target Hint.xcplaygroundpage/Contents.swift
@@ -6,5 +6,5 @@
 ---
 * We begin with a turtle who knows how to move `forward()` and `back()`
 * Can you steer the turtle to the target?
-* To reset the target, remove the turtle and type resetTarget()
+* To reset the target, remove everything from the playground including the turtle and type `resetTarget()`. Once the target is reset, remove `resetTarget()` and continue.
 */
